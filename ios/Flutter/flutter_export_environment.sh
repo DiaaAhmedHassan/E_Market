cf7bbae781ebdb7ff1f7edc92e6181c4aa2dfb87
DiaaAhmedHassan <diaalion06@gmail.com>
2024-09-11 17:26:22 +0300

    added firebase libs

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=E:\Programing\flutterSrc\flutter"
-export "FLUTTER_APPLICATION_PATH=E:\Programing\AndroidPros\e_market"
+export "FLUTTER_APPLICATION_PATH=E:\Programing\AndroidPros\e_flutter_market\e_market"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
